9ede832f51b1ace830a077f2a4ba622f4f290a05
GreenFlySun <tolikzaa2000@gmail.com>
2021-10-28 16:00:57 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import discord
-from discord.ext import commands
-from music_cog import music_cog
-bot = commands.Bot(command_prefix='!')
-bot.add_cog(music_cog(bot))
-
-bot.run('OTAyNTEwODUwNzMxMDQ5MDIx.YXfeug.zDxMrOI11ZRSNPQ1213hcXl_4Zc')
